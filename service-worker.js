61c1fddf2c2dc48e8d1dd2c17b3749a499eb061f
hausi06mh <sebastianh2204@gmail.com>
2026-08-04 13:24:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-const CACHE='projekt-nizza-v11-1-hotfix';
-const ASSETS=['./','./index.html','./styles.css?v=11.1','./app.js?v=11.1','./manifest.json?v=11.1','./icon-192.png','./icon-512.png','./europe-map.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  }).catch(()=>caches.match(event.request)));
-});
+const CACHE='projekt-nizza-12-0';const FILES=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
